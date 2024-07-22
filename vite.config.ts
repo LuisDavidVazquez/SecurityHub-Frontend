ae47cac30817d93579fd2bd66e83fe1c23faba8b
Eliseo <Eliseo10208@gmail.com>
2024-07-22 02:08:09 -0600

    Se arreglo el proyecto vite.configs para hacer el build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/fonts',
+          dest: 'assets/fonts'
+        },
+        {
+          src: 'src/assets/images',
+          dest: 'assets/images'
+        },
+        {
+          src: 'src/assets/svg',
+          dest: 'assets/svg'
+        }
+      ]
+    })
+  ]
+});
